644d8b51532aee0fa9eed89bbada86bffd303468
231B372 <231B372@juet.co.in>
2025-07-29 15:16:27 +0530

    Two-sum

--- LE1/sum_of_two_numbers.py
+++ LE1/sum_of_two_numbers.py
@@ -1,8 +1,4 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second nummber:"))
 sum=num1+num2
-pd=num1*num2
-if sum==10 and pd<20:
-    print("Incorrect")
-else:
-    print("Correct")
+print(sum)
